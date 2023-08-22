2ace7667f9392d37ad0d178e4b4e86c5cf17ab0a
Jo Seung Hyo <seunghyo7742@naver.com>
2023-08-22 18:43:01 +0900

    Setting Next.js - till tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import '../styles/globals.css'
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+        <h1 className=" font-bold underline">Hello, Next.js!</h1>
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
