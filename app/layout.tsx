aca6c98c79d7fdfc40671406a3f6557550227458
Jo Seung Hyo <seunghyo7742@naver.com>
2023-08-29 17:53:55 +0900

    Feat: Modal template

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from '@/components/Sidebar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
+import ModalProvider from '@/providers/ModalProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider/>
         <Sidebar>{children} </Sidebar>
       </body>
     </html>
