720ba3594c10b662d98c16595404528abcc3ace2
Jo Seung Hyo <seunghyo7742@naver.com>
2023-08-31 17:31:24 +0900

    Style: submit button

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -11,49 +11,45 @@ interface SignUpFormState {
 const page: React.FC<SignUpFormState> = () => {
   useEffect(() => console.log('Rendered'));
 
-  const [email, setEmail]= useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [check_pw, setCheckPw] = useState('');
 
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const checkPwRef = useRef<HTMLInputElement>(null);
-  
-  //유효성 검증
-  let isValidEmail = (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/).test(email);
-  let isValidPassword=(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/).test(password);
-  
-  
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-    const {name, value} = e.target;
-    if(name=== 'email'){
-      setEmail(value)
-    }
-    else if(name==='password'){
-      setPassword(value)
-    }
-    else if(name==="check_pw"){
-      setCheckPw(value)
-    }
-  }
 
-  const onSubmit=(e: React.FormEvent<HTMLFormElement>)=>{
+  //유효성 검증
+  let isValidEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(email);
+  let isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/.test(password);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    } else if (name === 'check_pw') {
+      setCheckPw(value);
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(isValidEmail && isValidPassword && password===check_pw){
-      console.log(`save email as ${email}`)
-      console.log(`save password as ${password}`)
+    if (isValidEmail && isValidPassword && password === check_pw) {
+      console.log(`save email as ${email}`);
+      console.log(`save password as ${password}`);
+    } else {
+      if (!isValidEmail) {
+        emailRef.current?.focus();
+      } else if (!isValidPassword) {
+        passwordRef.current?.focus();
+      } else {
+        checkPwRef.current?.focus();
+      }
     }
-    else{
-        if (!isValidEmail) {
-          emailRef.current?.focus();
-        } else if (!isValidPassword) {
-          passwordRef.current?.focus();
-        } else {
-          checkPwRef.current?.focus();
-        }
-    }
-  }
-  
+  };
+
   return (
     <div className="px-5 pt-3 h-[70%] w-[70%] border border-neutral-700  rounded-md bg-white ">
       <Typography className="text-neutral-800 heading" variant="h4">
@@ -68,10 +64,10 @@ const page: React.FC<SignUpFormState> = () => {
           placeholder="seunghyoJo@example.com"
           fullWidth
           margin="normal"
-          onChange= {handleInputChange}
+          onChange={handleInputChange}
           inputRef={emailRef}
           error={!isValidEmail}
-          helperText={isValidEmail? "":"이메일 형식에 맞춰주세요."}
+          helperText={isValidEmail ? '' : '이메일 형식에 맞춰주세요.'}
         />
         <TextField
           id="outlined-password-input"
@@ -84,8 +80,9 @@ const page: React.FC<SignUpFormState> = () => {
           onChange={handleInputChange}
           inputRef={passwordRef}
           error={!isValidPassword}
-          helperText=
-          {isValidPassword? '':"8자 이상, 특수문자 1개 이상, 영문 대소문자 1개 이상 포함"} 
+          helperText={
+            isValidPassword ? '' : '8자 이상, 특수문자 1개 이상, 영문 대소문자 1개 이상 포함'
+          }
         />
         <TextField
           id="outlined-password-input"
@@ -96,11 +93,11 @@ const page: React.FC<SignUpFormState> = () => {
           fullWidth
           margin="normal"
           onChange={handleInputChange}
-          error={!isValidPassword||password!==check_pw}
-          helperText={isValidPassword? '':"비밀번호를 한번 더 입력해주세요."}
+          error={!isValidPassword || password !== check_pw}
+          helperText={isValidPassword ? '' : '비밀번호를 한번 더 입력해주세요.'}
           inputRef={checkPwRef}
         />
-        <Button type="submit" variant="contained" color="success" >
+        <Button className="text-white bg-black hover:bg-emerald-800" type="submit" color="success">
           Sign Up
         </Button>
       </form>
