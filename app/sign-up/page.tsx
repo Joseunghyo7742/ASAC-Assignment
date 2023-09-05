3bb077e40227783ae183dab15468091e66af2254
Jo Seung Hyo <seunghyo7742@naver.com>
2023-09-05 15:36:28 +0900

    Error: error with ref array

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,71 +3,81 @@ import { useState, useRef } from 'react';
 import { Typography, TextField, Button } from '@mui/material';
 import { useModalDispatch, useModalState } from '@/components/ModalProvider';
 
-
-const SignUp_form_field=[
+const Signup_Form_Field = [
   {
-    id:"field1"
-    name:
-  }
-]
+    id: 'field1',
+    name: 'email',
+    label: 'Email',
+    placeholder: 'user@example.com',
+    pattern: '(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,3}$/)',
+    errorTitle:'Check Email',
+    errorMessage: 'Invalid email format!',
+  },
+  {
+    id: 'field2',
+    name: 'password',
+    label: 'Password',
+    placeholder: 'Password',
+    pattern: '(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,})',
+    errorTitle:'Check Password',
+    errorMessage: 'Invalid password format!',
+  },
+  {
+    id: 'field3',
+    name: 'confirm_password',
+    label: 'Confirm password',
+    placeholder: 'type your password again',
+    pattern: '(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,})',
+    errorTitle:'Check Password',
+    errorMessage: 'Password is unmatched!',
+  },
+];
+interface registerForm {
+  email: string;
+  password: string;
+  confirm_password:string;
+}
+const initialForm = {
+  email: '',
+  password: '',
+  confirm_password:''
+};
 
 const page = () => {
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [check_pw, setCheckPw] = useState('');
-
-  // const emailRef = useRef<HTMLInputElement>(null);
-  // const passwordRef = useRef<HTMLInputElement>(null);
-  // const checkPwRef = useRef<HTMLInputElement>(null);
-  const formInfo=[email,password,check_pw];
-  const  formRef=useRef<null[] | HTMLDivElement[]>([]);
-  
+  const [formValues, setFormValues] = useState<registerForm>(initialForm);
+  const inputTarget =  useRef<HTMLInputElement[] | null[]>([]);
   const dispatch = useModalDispatch();
   const state = useModalState();
-
-  //유효성 검증
-  const isValidEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(email);
-  const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/.test(password);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const { name, value } = e.target;
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    } else if (name === 'check_pw') {
-      setCheckPw(value);
+    for(const element in formValues){
+      if(element===name)
+        setFormValues({...formValues, [name]:value})
     }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    let modalTitle = '';
-    let modalMessage = '';
-    if (isValidEmail && isValidPassword && password === check_pw) {
-      console.log(`save email as ${email}`);
-      console.log(`save password as ${password}`);
-    } else if (!isValidEmail) {
-      emailRef.current?.focus();
-      modalTitle = 'Email Error';
-      modalMessage = 'Invalid Email formation. Check it again!';
-    } else if (!isValidPassword) {
-      passwordRef.current?.focus();
-      modalTitle = 'Password Error';
-      modalMessage = 'Invalid pssword formtion. Check it again!';
-    } else {
-      checkPwRef.current?.focus();
-      modalTitle = 'Password miss matched';
-      modalMessage = 'Confirm password again';
-    }
-    dispatch({
-      type: 'OPEN_MODAL',
-      payload: { title: modalTitle, message: modalMessage },
+    
+    const invalidField = inputTarget.current.find((ref: { pattern: string | RegExp; value: string; }) => {
+      const pattern = new RegExp(ref.pattern);
+      return !pattern.test(ref.value);
     });
+    
+    if (invalidField) {
+      // 패턴이 유효하지 않은 입력 필드에 포커스 주기
+      invalidField.focus();
+      dispatch({
+        type: 'OPEN_MODAL',
+        payload: {
+          title: {invalidField.errorTitle},
+          message: {invalidField.errorMessage},
+        },
+      });
       state.modalRef.current?.showModal();
+    
   };
 
   return (
@@ -76,49 +86,21 @@ const page = () => {
         Welcome!
       </Typography>
       <form onSubmit={handleSubmit}>
-      {formInfo.map((item, index)=>(
-        
-      ))}
-        <TextField
-          id="outlined-required "
-          name="email"
-          label="Email"
-          placeholder="seunghyoJo@example.com"
-          fullWidth
-          margin="normal"
-          onChange={handleInputChange}
-          inputRef={emailRef}
-          error={!isValidEmail}
-          helperText={isValidEmail ? '' : '이메일 형식에 맞춰주세요.'}
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          fullWidth
-          margin="normal"
-          onChange={handleInputChange}
-          inputRef={passwordRef}
-          error={!isValidPassword}
-          helperText={
-            isValidPassword ? '' : '8자 이상, 특수문자 1개 이상, 영문 대소문자 1개 이상 포함'
-          }
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Confirm password"
-          name="check_pw"
-          type="password"
-          autoComplete="current-password"
-          fullWidth
-          margin="normal"
-          onChange={handleInputChange}
-          error={!isValidPassword || password !== check_pw}
-          helperText={isValidPassword ? '' : '비밀번호를 한번 더 입력해주세요.'}
-          inputRef={checkPwRef}
-        />
+        {Signup_Form_Field.map((item, index) => (
+          <TextField
+            key={item.id}
+            name={item.name}
+            label={item.label}
+            placeholder={item.placeholder}
+            onChange={handleInputChange}
+            //element는 현재 요소를 참조 즉 TextField
+            inputRef={(element) => {
+              inputTarget.current[index] = element;
+            }}
+            margin="normal"
+            inputProps={{pattern:item.pattern, errorTitle:item.errorTitle, errorMessage:item.errorMessage}}
+          />
+        ))}
         <Button className="text-white bg-black hover:bg-emerald-800" type="submit" color="success">
           Sign Up
         </Button>
