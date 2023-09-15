67e71f06e1f064218e6a74600435c65f74262ce8
Jo Seung Hyo <seunghyo7742@naver.com>
2023-09-15 18:24:33 +0900

    Feat: get SearchResult

--- app/search-page/page.tsx
+++ app/search-page/page.tsx
@@ -3,12 +3,12 @@ import Header from '@/components/Header';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { TextField } from '@mui/material';
+import SearchResult from '@/app/search-page/SearchResult';
 
 const page = () => {
-  const [searchInput, setSearchInput] = useState(null);
   const [searchResult, setSearchResult] = useState(null);
   const [accessToken, setAccessToken] = useState('');
-  const CLIENT_ID = 'fa2cfe7869ff48e5b6267e74c530c493';
+  const CLIENT_ID = 'fa2cfe7869ff48e5b6267e74c530c493'; //env에 넣자
   const CLIENT_SECRET = process.env.NEXT_PUBLIC_SP_CLIENT_SECRET;
   useEffect(() => {
     axios({
@@ -28,17 +28,16 @@ const page = () => {
   }, []);
 
   //search
-  async function search() {
+  async function search(searchInput) {
     try {
       const response = await axios({
         method: 'get',
-        url: `https://api.spotify.com/v1/search?q=${searchInput}&type=track,artist,playlist,album`,
+        url: `https://api.spotify.com/v1/search?q=${searchInput}&type=track%2Calbum%2Cplaylist%2Cartist&offset=10&limit=15`,
         headers: { Authorization: 'Bearer  ' + accessToken },
       });
       setSearchResult(response);
-      console.log('get artist', response);
     } catch (error) {
-      console.log('Could not get artist data', error);
+      console.log('Could not get search result', error);
     }
   }
 
@@ -46,17 +45,17 @@ const page = () => {
     <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
       <Header>
         <TextField
-          className="text-white"
+          className="text-white "
           id="standard-basic"
           variant="standard"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              search();
-            }
+          onChange={(e)=>{
+            search(e.target.value)
           }}
-          onChange={(e) => setSearchInput(e.target.value)}
         />
       </Header>
+      <main>
+        <SearchResult searchResult={searchResult}/>
+      </main>
     </div>
   );
 };
